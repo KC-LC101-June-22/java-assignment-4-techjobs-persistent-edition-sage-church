64e1357e62ffa10c7b6d7a2b477d64945fed8f76
sage-church <sagin.churchman77@gmail.com>
2022-12-01 08:14:58 -0600

    task 2 nearly complete

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
 -- Part 1: Test it with SQL
+--      SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+--      WHERE table_schema = 'techjobs' AND table_name = 'job'
+
+--      RESULTS: id = int, employer = varchar, name = varchar, skills = varchar
 
 -- Part 2: Test it with SQL
 
