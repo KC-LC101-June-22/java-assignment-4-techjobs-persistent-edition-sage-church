2f6e57da5c0cc1506d10f081c67873c310630134
sage-church <sagin.churchman77@gmail.com>
2022-12-01 18:23:15 -0600

    task 3 nearly complete

--- queries.sql
+++ queries.sql
@@ -2,9 +2,9 @@
 --      SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
 --      WHERE table_schema = 'techjobs' AND table_name = 'job'
 
---      RESULTS: id = int, employer = varchar, name = varchar, skills = varchar
-
 -- Part 2: Test it with SQL
+
+--      SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
